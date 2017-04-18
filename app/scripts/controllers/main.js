5e6154be500de1a4ecc9774bcd76d1fb0db8acdf
liubin <2201318990@qq.com>
2017-04-18 20:57:12 +0800

    总项目

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name detwoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the detwoApp
+ */
+angular.module('detwoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
